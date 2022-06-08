022b162e2b39dadeb47fbfb4f2c7cd3c80ca1ff3
rebecalais <rebecaquirino2202@gmail.com>
2022-06-08 17:07:37 -0300

    Initial commit

---
+++ javascript/teste.js
@@ -0,0 +1,16 @@
+// var y = 0; comentário por linha
+var x = 1;
+/*var z = 2; várias linhas até fechar o comentário
+var p = 5;*/
+var r = 8;
+
+//Funções
+function soma(a, b) {
+    return a + b;
+} //declarando uma função
+
+soma(3, 5); //chamando a função
+
+// Console
+
+console.log ("Hello world");
